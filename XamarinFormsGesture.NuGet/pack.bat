fba30157e60b8f57344f2b898d4372e9c8d8c3eb
Benjamin Mayrargue <benjamin@vapolia.fr>
2017-08-03 16:09:38 +0200

    version + nuget for uw

--- XamarinFormsGesture.NuGet/pack.bat
+++ XamarinFormsGesture.NuGet/pack.bat
@@ -9,7 +9,7 @@
 set nugetServer=https://www.nuget.org/api/v2/package/
 
 del *.nupkg
-set version=1.1.1-pre3
+set version=1.1.1-pre4
 nuget pack XamarinFormsGesture.nuspec -Version "%version%"
 nuget push Vapolia.XamarinFormsGesture.*.nupkg -Source %nugetServer%
 
